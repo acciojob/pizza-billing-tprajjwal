f4a23eb560a134bc3c65a901c8fcad72c53c0687
Prajjwal Tripathi <tprajjwal22@gmail.com>
2023-03-22 03:06:05 +0530

    8thtry

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,78 +1,104 @@
 package com.driver;
 
 public class Pizza {
-    private int price;
-    private int cheesePrice;
-    private int toppingPrice;
-    private int paperBagPrice;
-    private String bill;
-    private boolean isVeg;
-    private boolean isCheeseAdded;
-    private boolean isToppingAdded;
-    private boolean isPaperBagAdded;
-    private boolean isBillCreated;
-    public Pizza(boolean isVeg) {
-        this.isVeg = isVeg;
-        isCheeseAdded=false;
-        isToppingAdded=false;
-        isPaperBagAdded=false;
-        isBillCreated=false;
-        bill="";
+
+    public int cheesePrice;
+
+    public int basePrice;
+
+    public int toppingsPrice;
+
+    public int paperBagPrice;
+
+    public String bill;
+
+    public int totalPrice;
+
+    boolean isCheeseAdded;
+    // 2 benefits of these
+    //1. It prevents duplicate addition of the cheese if called multiple times
+    //2. It also tells which all things have been added
+
+
+    boolean isToppingsAdded;
+
+    boolean isPaperBagAdded;
+
+    boolean isBillGenerated;
+
+    public Pizza(Boolean isVeg){
+
+        //Constructor is used to
+        //initialize the attributes of any class
 
         if(isVeg){
-            this.price=300;
-            this.toppingPrice=70;
+            basePrice = 300;
+            toppingsPrice = 70;
+        }else{
+            //non veg case
+            basePrice = 400;
+            toppingsPrice = 120;
         }
-        else {
-            this.price=400;
-            this.toppingPrice=120;
-        }
+        paperBagPrice = 20;
+        cheesePrice = 80;
 
-        this.bill+="Base Price of Pizza: "+this.price+"\n";
+        isCheeseAdded = false;
+        isToppingsAdded = false;
+        isPaperBagAdded = false;
+
+
+        totalPrice = basePrice;
+        bill = "Base Price Of The Pizza: "+basePrice + "\n";
     }
 
     public int getPrice(){
-        return this.price;
+        return totalPrice;
     }
 
     public void addExtraCheese(){
-        if(!isCheeseAdded){
-            this.price+=+80;
-            isCheeseAdded=true;
+
+        if(isCheeseAdded==false){
+            totalPrice = totalPrice + cheesePrice;
+            isCheeseAdded= true; //So to prevent it from adding again
         }
 
     }
+
     public void addExtraToppings(){
-        if(!isToppingAdded){
-            this.price+=this.toppingPrice;
-            isToppingAdded=true;
+
+        if(isToppingsAdded==false){
+            totalPrice = totalPrice + toppingsPrice;
+            isToppingsAdded = true;
         }
     }
+
     public void addTakeaway(){
-        if (!isPaperBagAdded) {
 
-            this.price+=20;
-            isPaperBagAdded=true;
+        if(isPaperBagAdded==false){
+            totalPrice = totalPrice + paperBagPrice;
+            isPaperBagAdded = true;
         }
     }
+
     public String getBill(){
-        if(!isBillCreated) {
-            if (isCheeseAdded) {
-                this.bill += "Extra Cheese Added: 80" + "\n";
+
+        if(isBillGenerated==false){
+
+
+            if(isCheeseAdded==true){
+                bill = bill + "Extra Cheese Added: "+cheesePrice + "\n";
             }
-            if (isToppingAdded) {
-                this.bill += "Extra Toppings Added: " + this.toppingPrice + "\n";
+            if(isToppingsAdded==true){
+                bill = bill + "Extra Toppings Added: "+toppingsPrice + "\n";
             }
-            if (isPaperBagAdded) {
-                this.bill += "Paperbag Added: 20"+"\n";
+            if(isPaperBagAdded==true){
+                bill = bill + "Paperbag Added: "+paperBagPrice + "\n";
             }
-            this.bill += "Total Price: "+this.price +"\n";
-            isBillCreated = true;
-            return this.bill;
+
+            bill = bill + "Total Price: "+totalPrice + "\n";
+
+            isBillGenerated = true;
         }
-        return this.bill;
+        return bill;
     }
-
-
-
 }
